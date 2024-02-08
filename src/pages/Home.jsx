454c30ca467680ddb5495caae5c1e8ca294067e6
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-02-08 23:06:42 +0300

    feat: Router structure added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      Home
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nobis saepe
+        veniam, possimus id voluptate optio voluptates sunt quae perspiciatis,
+        quam excepturi fugit enim accusamus eligendi tempora. Autem, cum
+        architecto?
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import { BrowserRouter,Routes, Route } from 'react-router-dom'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import PrivateRouter from "./PrivateRouter";
+import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
+import Firms from "../pages/Firms";
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
+
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Login/>} />
-            <Route path='register' element={<Register />} />
-            
-        </Routes>
-    </BrowserRouter>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="stock" element={<PrivateRouter />}>
+          <Route path="" element={<Dashboard />} >
+            <Route index path="" element={<Home />} />                                 
+            <Route path="purchases" element={<Purchases />} />                                 
+            <Route path="sales" element={<Sales />} />                                 
+            <Route path="firms" element={<Firms />} />                                 
+            <Route path="brands" element={<Brands />} />                                 
+            <Route path="products" element={<Products />} />                                 
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
