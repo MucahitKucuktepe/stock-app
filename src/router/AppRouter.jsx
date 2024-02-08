d51cd052f78da53c48f86bb595d2d48bcb43e0b3
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-02-08 22:16:15 +0300

    feat: login pages added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { BrowserRouter,Routes, Route } from 'react-router-dom'}
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Login from '../pages/Login'
 const AppRouter = () => {
   return (
     <BrowserRouter>
         <Routes>
-            
+            <Route path='/' element={<Login/>} />
         </Routes>
     </BrowserRouter>
   )
